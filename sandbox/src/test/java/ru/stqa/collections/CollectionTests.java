f76022a91a08c69c5d98b6fdf636b9c1f3d6de97
ntaranko <ntaranko@gmail.com>
2024-03-02 20:57:39 +0300

    Lecture 4.1 - Array vs ArrayList

---
+++ sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
@@ -0,0 +1,31 @@
+package ru.stqa.collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionTests {
+
+    @Test
+    void arrayTests(){
+        var array = new String[]{"a", "b", "c"};
+        array[0]="a";
+        Assertions.assertEquals("a", array[0]);
+
+        array[0] = "d";
+        Assertions.assertEquals("d", array[0]);
+    }
+
+    @Test
+    void listTest(){
+
+        var list = new ArrayList<String>(List.of("a", "b", "c"));
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals("a", list.get(0));
+
+        list.set(0,"d");
+        Assertions.assertEquals("d", list.get(0));
+    }
+}
