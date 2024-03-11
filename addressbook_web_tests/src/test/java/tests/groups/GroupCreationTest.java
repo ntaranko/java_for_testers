f542418165739a75ece2e9d8f07199c0dc158a06
ntaranko <ntaranko@gmail.com>
2024-03-11 21:56:52 +0300

    Lecture 5.4 - Save test data in JSON

--- addressbook_web_tests/src/test/java/tests/groups/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/tests/groups/GroupCreationTest.java
@@ -1,5 +1,7 @@
 package tests.groups;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import common.CommonFunctions;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
@@ -7,6 +9,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import tests.TestBase;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -34,6 +38,38 @@ public class GroupCreationTest extends TestBase {
     @ParameterizedTest
     @MethodSource("groupProvider")
     public void canCreateMultipleGroup(GroupData group) {
+        var oldGroups = app.groups().getList();
+        app.groups().createGroup(group);
+        var newGroups = app.groups().getList();
+        Comparator<GroupData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newGroups.sort(compareById);
+
+        var expectedList = new ArrayList<>(oldGroups);
+        expectedList.add(group.withId(newGroups.get(newGroups.size()-1).id()).withHeader("").withFooter(""));
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newGroups,expectedList);
+    }
+
+    public static List<GroupData> jsonGroupProvider() throws IOException {
+        var result = new ArrayList<GroupData>();
+        /*for (var name : List.of("", "group name")) {
+            for (var header : List.of("", "group header")) {
+                for (var footer : List.of("", "group footer")) {
+                    result.add(new GroupData().withName(name).withHeader(header).withFooter(footer));
+                }
+            }
+        }*/
+        ObjectMapper mapper = new ObjectMapper();
+        var value = mapper.readValue(new File("groups.json"), new TypeReference<List<GroupData>>(){});
+        result.addAll(value);
+        return result;
+    }
+
+    @ParameterizedTest
+    @MethodSource("jsonGroupProvider")
+    public void canCreateMultipleGroupFromJson(GroupData group) {
         var oldGroups = app.groups().getList();
         app.groups().createGroup(group);
         var newGroups = app.groups().getList();
