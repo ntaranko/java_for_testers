2d2e51725e23b20715b646dd03f76304385570bf
ntaranko <ntaranko@gmail.com>
2024-03-24 12:58:20 +0300

    Lecture 7.3 - Transformers

--- addressbook_web_tests/src/test/java/common/CommonFunctions.java
+++ addressbook_web_tests/src/test/java/common/CommonFunctions.java
@@ -1,14 +1,29 @@
 package common;
 
 import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CommonFunctions {
-    public static String randomString(int n) {
+    public static String randomStringOld(int n) {
         var rnd = new Random();
         var result = "";
         for (int i = 0; i < n; i++) {
             result = result + (char) ('a' + rnd.nextInt(26));
         }
+
+        return result;
+    }
+
+    public static String randomString(int n) {
+        var rnd = new Random();
+        Supplier<Integer> randomNumbers = () -> rnd.nextInt(26);
+       var result = Stream.generate(randomNumbers)
+                .limit(n)
+                .map(i -> 'a' + i)
+                .map(Character::toString)
+                .collect(Collectors.joining());
 
         return result;
     }
@@ -20,7 +35,7 @@ public class CommonFunctions {
             result = result + (char) ('a' + rnd.nextInt(26));
         }
 
-        if (result.equals("")){
+        if (result.equals("")) {
             return result;
         }
         return result + "@gmail.com";
--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -8,8 +8,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HibernateHelper extends HelperBase {
     private SessionFactory sessionFactory;
@@ -27,11 +27,7 @@ public class HibernateHelper extends HelperBase {
     }
 
     static List<GroupData> convertGroupList(List<GroupRecord> records) {
-        List<GroupData> result = new ArrayList<>();
-        for (var record : records) {
-            result.add(convertGroup(record));
-        }
-        return result;
+        return records.stream().map(HibernateHelper::convertGroup).collect(Collectors.toList());
     }
 
     private static GroupData convertGroup(GroupRecord record) {
@@ -67,11 +63,7 @@ public class HibernateHelper extends HelperBase {
     }
 
     static List<ContactData> convertContactList(List<ContactRecord> records) {
-        List<ContactData> result = new ArrayList<>();
-        for (var record : records) {
-            result.add(convertContact(record));
-        }
-        return result;
+        return records.stream().map(HibernateHelper::convertContact).collect(Collectors.toList());
     }
 
     private static ContactData convertContact(ContactRecord record) {
