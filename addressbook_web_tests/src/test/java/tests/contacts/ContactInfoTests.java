bfd1b0e61ad386bdec80c3d9a1b50fb3e8e04493
ntaranko <ntaranko@gmail.com>
2024-03-28 13:03:41 +0300

    Task 16 - Fix: Combine tests

--- addressbook_web_tests/src/test/java/tests/contacts/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/contacts/ContactInfoTests.java
@@ -5,7 +5,6 @@ import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
-
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -36,7 +35,7 @@ public class ContactInfoTests extends TestBase {
     }
 
     @Test
-    void testEmails() {
+    void testContactData() {
         if (app.hbm().getContactCount() == 0) {
             app.contacts().createContact(new ContactData()
                     .withFirstName(CommonFunctions.randomString(5))
@@ -51,31 +50,22 @@ public class ContactInfoTests extends TestBase {
         }
         var contacts = app.hbm().getContactList();
         var contact = contacts.get(0);
-        var emails = app.contacts().getEmails(contact);
-        var expected = Stream.of(contact.email(), contact.email2(), contact.email3())
+
+        var phones = app.contacts().getPhones(contact);
+        var expectedPhones = Stream.of(contact.homePhone(), contact.mobilePhone(), contact.workPhone())
                 .filter(s -> s != null && !"".equals(s))
                 .collect(Collectors.joining("\n"));
-        Assertions.assertEquals(expected, emails);
-    }
+        Assertions.assertEquals(expectedPhones, phones);
 
-    @Test
-    void testAddress() {
-        if (app.hbm().getContactCount() == 0) {
-            app.contacts().createContact(new ContactData()
-                    .withFirstName(CommonFunctions.randomString(5))
-                    .withLastName(CommonFunctions.randomString(10))
-                    .withAddress(CommonFunctions.randomString(15))
-                    .withHomePhone(CommonFunctions.randomString(12))
-                    .withMobilePhone(CommonFunctions.randomString(12))
-                    .withWorkPhone(CommonFunctions.randomString(12))
-                    .withEmail(CommonFunctions.randomEmail(10))
-                    .withEmail2(CommonFunctions.randomEmail(10))
-                    .withEmail3(CommonFunctions.randomEmail(10)));
-        }
-        var contacts = app.hbm().getContactList();
-        var contact = contacts.get(0);
+        var emails = app.contacts().getEmails(contact);
+        var expectedEmails = Stream.of(contact.email(), contact.email2(), contact.email3())
+                .filter(s -> s != null && !"".equals(s))
+                .collect(Collectors.joining("\n"));
+        Assertions.assertEquals(expectedEmails, emails);
+
         var address = app.contacts().getAddress(contact);
-        var expected = contact.address();
-        Assertions.assertEquals(expected, address);
+        var expectedAddress = contact.address();
+        Assertions.assertEquals(expectedAddress, address);
+
     }
 }
